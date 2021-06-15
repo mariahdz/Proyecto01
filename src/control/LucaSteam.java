d9a7b0018a6376bfde0a09028219ad421b2c3050
unknown <mariahdeztejero@gmail.com>
2021-06-15 17:30:23 +0200

    Conflict solved

--- src/control/LucaSteam.java
+++ src/control/LucaSteam.java
@@ -1,5 +1,41 @@
 package control;
 
+import gui.Menu;
+import servicios.ServiciosLucaSteam;
+import servicios.ServiciosLucaSteamImp;
+import utilidades.RecogerDatos;
+
+/**
+* Clase: LucaSteam <br>
+* Descripción: Esta clase sirve dar inicio al prog
+* @author María Hernández
+* @version 15/06/2021/A
+*/
 public class LucaSteam {
 
+	private ServiciosLucaSteam inventario = new ServiciosLucaSteamImp();
+	
+	public void iniciarLucaSteam() {
+		boolean continuar = true;
+		do {
+			Menu.mostrarMenu();
+			continuar = this.seleccionOpciones();
+		}while(continuar=true);
+		System.out.println();
+	}
+	
+	public boolean seleccionOpciones() {
+		
+		boolean continuar=true;
+		
+		try {
+			switch (RecogerDatos.recogeInt()) {
+				
+				case 1:
+					//Alta de un juego
+					
+					
+			}
+		}
+	}
 }
