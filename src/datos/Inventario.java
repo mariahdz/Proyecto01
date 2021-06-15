d3cdb2520410bcd3213a059039ac4f1d83a4c28a
unknown <mariahdeztejero@gmail.com>
2021-06-15 14:01:05 +0200

    Crear InventarioImp e interfaz. Método addJuego y método existePk

--- src/datos/Inventario.java
+++ src/datos/Inventario.java
@@ -1,5 +1,16 @@
 package datos;
 
+import model.Juego;
+
+/**
+ * 
+ * @author María Hernández
+ * @version 15/06/2021/A
+ *
+ */
 public interface Inventario {
 
+	public boolean addJuego(int pk, Juego j);
+	public boolean existePk(int pk);
+	
 }
--- src/datos/InventarioImp.java
+++ src/datos/InventarioImp.java
@@ -1,5 +1,68 @@
 package datos;
 
-public class InventarioImp {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import model.Juego;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * <p>
+ * @author María Hernández
+ * @version 15/06/2021/A
+ * </p>
+*/
+public class InventarioImp implements Inventario {
+
+	private Map<Integer, Juego> inventario= new HashMap<>();
+	
+	public Map<Integer, Juego> getInventario(){
+		return inventario;
+	}
+	
+	public void setInventario(Map<Integer, Juego> inventario) {
+		this.inventario=inventario;
+	}
+
+	/*
+	 * MÉTODOS
+	 */
+	/**
+	 * <p>
+	 * Método que añade un juego nuevo al inventario.
+	 * @param pk - Primary Key
+	 * @param j - Juego
+	 * @return boolean
+	 * </p>
+	 */
+	public boolean addJuego (int pk, Juego j) {
+		if(inventario.containsKey(pk)) {
+			System.out.println("No se puede guardar el juego. El código está repetido. ");
+			return false;
+		}else {
+			inventario.put(pk, j);
+			return true;
+		}
+	}
+	
+	/**
+	 *<p>
+	 * Método que devuelve si existe un registro. 
+	 * @param pk- Primary Key
+	 * @return
+	 * </p>
+	 */
+	public boolean existePk(int pk) {
+		return inventario.containsKey(pk);
+	}
+
+
 
 }
+
